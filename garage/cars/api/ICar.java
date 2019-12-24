5c8cd3d2f8e402c68a6b9e0e707270c0491ab72a
Andrey Kashpar <c_a_s@list.ru>
2019-12-24 07:44:43 +0300

    commit 21.12

---
+++ garage/cars/api/ICar.java
@@ -0,0 +1,14 @@
+package garage.cars.api;
+
+public interface ICar {
+    void drive();
+    void open();
+    void close();
+    void start();
+    void stop();
+    void fuel();
+    void changeWheel();
+    void driverSeat();
+    void passengerSeat();
+
+}
