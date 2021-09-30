899600e6b4bb2bb97d6fb01a11cd9cce54683097
Andrey Kashpar <c_a_s@list.ru>
2021-09-30 23:39:16 +0300

    Semaphore add

--- thread/CountDownMain.java
+++ thread/CountDownMain.java
@@ -1,31 +1,24 @@
 package thread;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class CountDownMain {
-    private static int VAL=3;
+    private static int VAL=5;
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch countDownLatch=new CountDownLatch(VAL);
         List<Integer>out= Collections.synchronizedList(new ArrayList<>());
         ExecutorService executorService= Executors.newFixedThreadPool(3);
-        for(int i = 0; i<3; i++){
+        for(int i = 0; i<8; i++){
         executorService.execute(new Worker(countDownLatch,out,i));}
-        for (int i = 0; i <VAL; i++) {
-            countDownLatch.countDown();
-        }
-        System.out.println("CountDownLatch worked out");
+        countDownLatch.await();
         executorService.shutdown();
-        for (Integer x:out
-             ) {
-            System.out.println(x);
-        }
+        System.out.println("CountDownLatch worked out");
+        System.out.println(out);
+
     }
 
 }
@@ -42,16 +35,16 @@ class Worker implements Runnable {
 
     @Override
     public void run() {
-        while (true) {
-            for (int i = 0; i < 100; i++) {
-                output.add(i);
-            }
-            try {
-                countDownLatch.await();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                output.add(value);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        countDownLatch.countDown();
+
             }
 
         }
-    }
-}
+
